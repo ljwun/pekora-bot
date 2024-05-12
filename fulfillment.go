e528c7569efc051d7defcf79702d9b60b0f15f61
ljwun <ljwun@yyuk-dev.link>
2024-05-13 00:23:48 +0800

    refactor(dialogflow_backend): Make code clean

--- fulfillment.go
+++ fulfillment.go
@@ -9,90 +9,52 @@ import (
 	"github.com/golang/protobuf/jsonpb"
 	"github.com/sirupsen/logrus"
 	"google.golang.org/genproto/googleapis/cloud/dialogflow/v2"
+	"google.golang.org/protobuf/types/known/structpb"
+)
+
+const (
+	parameter_id_member_name    string = "holoname"
+	parameter_id_specified_time string = "date-time"
+	field_id_start_date         string = "startDate"
+	field_id_end_date           string = "endDate"
+	field_id_start_time         string = "startTime"
+	field_id_end_time           string = "endTime"
+	field_id_start_date_time    string = "startDateTime"
+	field_id_end_date_time      string = "endDateTime"
 )
 
 func handleWebhook(c *gin.Context) {
 	wReq := dialogflow.WebhookRequest{}
-
 	if err := jsonpb.Unmarshal(c.Request.Body, &wReq); err != nil {
 		logrus.WithError(err).Error("Couldn't Unmarshal request to jsonpb")
 		c.Status(http.StatusBadRequest)
 		return
 	}
-	fmt.Printf("%10s : %+v\n", "Intent", wReq.QueryResult.Intent.DisplayName)
-	fmt.Printf("%10s : %+v\n", "Parameter", wReq.QueryResult.Parameters)
-	fmt.Printf("%10s : %+v\n", "Text", wReq.QueryResult.QueryText)
-	msg := fmt.Sprintln("Intent:", wReq.QueryResult.Intent.DisplayName)
+	logrus.WithFields(
+		logrus.Fields{
+			"intent":    wReq.QueryResult.Intent.DisplayName,
+			"parameter": wReq.QueryResult.Parameters,
+			"text":      wReq.QueryResult.QueryText,
+		},
+	).Debug("receive request from dialogflow")
 	params := wReq.QueryResult.Parameters.Fields
+	var msg string
 	switch wReq.QueryResult.Intent.DisplayName {
 	case "開台詢問":
-		//成員
-		members := []string{}
-		if params["holoname"].GetStringValue() != "" {
-			members = append(members, params["holoname"].GetStringValue())
-		} else {
-			for _, member := range params["holoname"].GetListValue().Values {
-				members = append(members, member.GetStringValue())
-			}
+		memberNames := parseMember(params)
+		specifiedTime, err := parseSpecifiedTime(params)
+		if err != nil {
+			logrus.WithError(err).Error("Couldn't parse specified time")
+			c.AbortWithStatusJSON(http.StatusBadRequest, err)
+			return
 		}
-		// msg = fmt.Sprintf("%sNames:%s\n", msg, strings.Join(members, ","))
-		//時間
-		if params["date-time"].GetStringValue() != "" {
-			// msg = fmt.Sprintf("%sdatetime:%s",msg, params["date-time"].GetStringValue())
-			fmt.Println("now ", params["date-time"].GetStringValue())
-			sTime, _ := time.Parse(time.RFC3339, params["date-time"].GetStringValue())
-			message, err := getSchedule(members, sTime)
-			if err != nil {
-				c.AbortWithError(http.StatusBadRequest, err)
-			}
-			fmt.Println(message)
-			msg = message
-		} else {
-			datetime := struct {
-				start string
-				end   string
-			}{}
-			switch {
-			case params["date-time"].GetStructValue().Fields["startDate"].GetStringValue() != "":
-				datetime.start = params["date-time"].GetStructValue().Fields["startDate"].GetStringValue()
-				datetime.end = params["date-time"].GetStructValue().Fields["endDate"].GetStringValue()
-			case params["date-time"].GetStructValue().Fields["startDateTime"].GetStringValue() != "":
-				datetime.start = params["date-time"].GetStructValue().Fields["startDateTime"].GetStringValue()
-				datetime.end = params["date-time"].GetStructValue().Fields["endDateTime"].GetStringValue()
-			case params["date-time"].GetStructValue().Fields["startTime"].GetStringValue() != "":
-				datetime.start = params["date-time"].GetStructValue().Fields["startTime"].GetStringValue()
-				datetime.end = params["date-time"].GetStructValue().Fields["endTime"].GetStringValue()
-			default:
-				fmt.Println("Magic vault: ", params["date-time"])
-			}
-			start, _ := time.Parse(time.RFC3339, datetime.start)
-			end, _ := time.Parse(time.RFC3339, datetime.end)
-			// msg = fmt.Sprintf("%sfrom:\n%v\nto:\n%v", msg, datetime.start, datetime.end)
-			if !start.IsZero() && !end.IsZero() {
-				if start.Before(end) {
-					message, err := getSchedule(members, start, end)
-					if err != nil {
-						c.AbortWithError(http.StatusBadRequest, err)
-					}
-					fmt.Println(message)
-					msg = message
-				} else {
-					message, err := getSchedule(members, end, start)
-					if err != nil {
-						c.AbortWithError(http.StatusBadRequest, err)
-					}
-					fmt.Println(message)
-					msg = message
-				}
-			} else {
-				message, err := getSchedule(members, time.Now())
-				if err != nil {
-					c.AbortWithError(http.StatusBadRequest, err)
-				}
-				fmt.Println(message)
-				msg = message
-			}
+		message, err := getSchedule(memberNames, specifiedTime...)
+		if err != nil {
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
 		}
+		logrus.WithField("message", message).Debug("success generate message")
+		msg = message
 	case "webhookDemo":
 		msg = "2"
 	default:
@@ -117,6 +79,56 @@ func handleWebhook(c *gin.Context) {
 	if err != nil {
 		c.AbortWithError(http.StatusBadRequest, err)
 	}
+}
+
+func parseMember(params map[string]*structpb.Value) []string {
+	result := make([]string, 0, 1)
+	membersParam := params[parameter_id_member_name]
+	if name := membersParam.GetStringValue(); name != "" {
+		result = append(result, name)
+	} else if namesValues := membersParam.GetListValue(); namesValues != nil {
+		for _, nameValue := range namesValues.Values {
+			if nameValue == nil {
+				continue
+			}
+			name := nameValue.String()
+			if name == "" {
+				continue
+			}
+			result = append(result, nameValue.String())
+		}
+	}
+	return result
+}
+
+func parseSpecifiedTime(params map[string]*structpb.Value) ([]time.Time, error) {
+	datetime := params[parameter_id_specified_time].GetStructValue()
+	if datetime == nil {
+		return []time.Time{time.Now()}, nil
+	}
+	var startString, endString string = "", ""
+	startString = datetime.Fields[field_id_start_date].GetStringValue()
+	endString = datetime.Fields[field_id_end_date].GetStringValue()
+	if startString == "" || endString == "" {
+		startString = datetime.Fields[field_id_start_time].GetStringValue()
+		endString = datetime.Fields[field_id_end_time].GetStringValue()
+	}
+	if startString == "" || endString == "" {
+		startString = datetime.Fields[field_id_start_date_time].GetStringValue()
+		endString = datetime.Fields[field_id_end_date_time].GetStringValue()
+	}
+	if startString == "" || endString == "" {
+		return []time.Time{time.Now()}, nil
+	}
+	start, err := time.Parse(time.RFC3339, startString)
+	if err != nil {
+		return nil, fmt.Errorf("fail to parse %s, err=%w", startString, err)
+	}
+	end, err := time.Parse(time.RFC3339, endString)
+	if err != nil {
+		return nil, fmt.Errorf("fail to parse %s, err=%w", endString, err)
+	}
+	return []time.Time{start, end}, nil
 }
 
 //超出時間表的範圍	=>	PEKORA我只知道最近這三天喔
